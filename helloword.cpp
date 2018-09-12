488fa3dd66c17e8feddbe46570733121924930c3
Hudson Schumaker <hudson@schumakerteam.com>
2018-09-11 22:20:15 -0300

    Hello World! c++.

---
+++ helloword.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: hudson schumaker
+ *
+ * Created on September 11, 2018, 9:58 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << "Friends!" << endl;
+    
+    //Variables
+    int numGatos = 6;
+    cout << "Gatos: " << numGatos << endl;
+    
+    return 0;
+}
