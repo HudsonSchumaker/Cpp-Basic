8d09998f57da1ae6685ece11c8fb8486f90990c9
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-13 18:34:50 +0200

    Cpp - unary

---
+++ unary_operator/Main.cpp
@@ -0,0 +1,34 @@
+//
+// Hudson Schumaker
+//
+
+#include <iostream>
+#include <string>
+
+int main() {
+
+    int a = 1;
+    ++a; // result: 2
+    a--; // result: 1
+    int minusa =- a; // result: -1
+    bool b = true;
+    !b; // result: true
+    
+    a =  4;
+    int c = a++/2; // equal to: (a==4) 4 / 2 result: 2 ('a' incremented postfix)
+    std::cout << a << std::endl; // prints 5!
+    
+    int d = ++a/2; // equal to: (a+1) == 6 / 2 result: 3
+    int arr[4] = {1,2,3,4};
+    
+    int *ptr1 = &arr[0]; // points to arr[0] which is 1
+    
+    int *ptr2 = ptr1++; // ptr2 points to arr[0] which is still 1; ptr1 incremented
+    std::cout << *ptr1++ << std::endl; // prints 2
+    
+    int e = arr[0]++; // receives the value of arr[0] before it is incremented
+    std::cout << e << std::endl; // prints 1
+    std::cout << *ptr2 << std::endl; // prints arr[0] which is now 2
+
+    return 0;
+}
