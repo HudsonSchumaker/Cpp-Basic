9ea2f3b82c80c188655ad06ce5b4a20aa0e8e997
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-14 19:29:24 +0200

    Cpp - pointer

---
+++ pointer/Main.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream> 
+
+int main() {
+    int var = 20;
+    int *ptr;
+    ptr = &var;
+    
+    // Outputs 20 (The value of var)
+    std::cout << var << std::endl;
+    
+    // Outputs 0x234f119 (var's memory location)
+    std::cout << ptr << std::endl;
+  
+    // Outputs 20 The value of the variable stored in the pointer ptr
+    std::cout << *ptr << std::endl;
+
+    return 0;
+} 
+
