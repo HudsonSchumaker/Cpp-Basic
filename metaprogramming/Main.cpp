3e3ffbc0a0b6c4f04d7785e47159f2c1a5c2a37e
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-14 19:16:06 +0200

    Cpp - factorial

---
+++ metaprogramming/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+template<unsigned int n>
+struct factorial {
+    enum {
+        value = n * factorial<n - 1>::value
+    };
+};
+template<>
+struct factorial<0> {
+    enum { value = 1 };
+};
+
+int main() {
+    std::cout << factorial<7>::value << std::endl; // prints "5040"
+}
