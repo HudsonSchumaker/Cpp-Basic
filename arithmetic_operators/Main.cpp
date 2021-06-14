4225af76794ceda3e94ca109bf9595dd0c717fad
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-14 19:24:06 +0200

    Cpp - fix bug

--- arithmetic_operators/Main.cpp
+++ arithmetic_operators/Main.cpp
@@ -7,6 +7,9 @@
 
 int main() {
 
+    float pi = 3.1415;
+    float R = 4.0;
+    float r = 2.0;
     // volume of a spherical shell = 4 pi R^3 - 4 pi r^3
     double vol = 4.0*pi*R*R*R/3.0 - 4.0*pi*r*r*r/3.0;
     
@@ -20,7 +23,7 @@ int main() {
     
     //Division and Modulo
     int g = 3-3%1; // equal to: 3 % 1 = 0 3 - 0 = 3
-    int h = 3-(3%1); // equal to: 3 % 1 = 0 3 - 0 = 3GoalKicker.com – C++ Notes for Professionals 16
+    int h = 3-(3%1); // equal to: 3 % 1 = 0 3 - 0 = 3
     int i = 3-3/1%3; // equal to: 3 / 1 = 3 3 % 3 = 0 3 - 0 = 3
     int l = 3-(3/1)%3; // equal to: 3 / 1 = 3 3 % 3 = 0 3 - 0 = 3
     int m = 3-(3/(1%3)); // equal to: 1 % 3 = 1 3 / 1 = 3 3 - 3 = 0
