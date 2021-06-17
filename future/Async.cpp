8292eb136550f23f62cc7756ecf1d5a93e6d5ba8
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-06-17 10:11:22 +0200

    Update Async.cpp

--- future/Async.cpp
+++ future/Async.cpp
@@ -4,8 +4,7 @@
 
 long asyncFunction ();
 
-int main()
-{
+int main() {
     std::future<long> fut = std::async(asyncFunction);
     std::cout << "doing something"<< std::endl;
     std::cout << "doing something else"<< std::endl;
@@ -14,8 +13,7 @@ int main()
     return 0;
 }
 
-long asyncFunction()
-{
+long asyncFunction() {
     std::cout << "starting asyncFunction ... " << std::endl;
     long max = 0;
     for (long i = 0; i < 1000000000; ++i)
