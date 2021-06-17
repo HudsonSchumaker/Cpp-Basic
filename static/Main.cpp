7e15ec2f7fddc2fe0659de9d34b5aeab270dec46
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-17 20:52:46 +0200

    Cpp - static

---
+++ static/Main.cpp
@@ -0,0 +1,20 @@
+//
+// Hudson Schumaker
+//
+
+#include <iostream>
+
+class Number {
+    private:
+        static const int accessCount = 0;
+
+    public:
+        static int zero(){return 0;}
+        static int getCount() { return accessCount; }
+};
+
+
+int main() {
+    Number::zero();
+    std::cout << Number::getCount() << std::endl;
+}
