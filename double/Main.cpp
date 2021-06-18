3796cc1f9ab9eff23908c861f40166bede3abdc5
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-06-18 11:13:31 +0200

    Cpp - double

---
+++ double/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  
+  double d = 994.50;
+  
+  cout.precision(2); // two units after . 
+  cout << fixed;     // keep when .00
+  
+  cout << "Number " << d << endl;
+  
+  return 0; 
+}
