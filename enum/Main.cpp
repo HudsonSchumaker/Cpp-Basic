f683809dedde0d1bddde6246d171425a56873e7d
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-06-22 11:45:33 +0200

    Cpp -  add enum

---
+++ enum/Main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+class Person final {
+    public:
+        enum Gender {
+            Male,
+            Female
+        };
+};
+
+int main() {
+    Person p = Person();
+    Person::Gender gender = p.Gender::Male;
+    
+    switch (gender) {
+        case Person::Gender::Male:
+            std::cout << "Gender is Male";
+            break;
+        
+        case Person::Gender::Female:
+            std::cout << "Gender is Female"; 
+            break;
+        
+        default:
+            std::cout << "Value can be Male or Female";
+   }
+   
+   return 0;
+}
