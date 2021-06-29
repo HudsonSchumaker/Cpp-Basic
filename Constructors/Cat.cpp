9a35dd6fa8f5b91abda932bab3c388838e8b2f62
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-29 20:51:15 +0200

    Cpp - change Cat

---
+++ Constructors/Cat.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Cat.hpp"
+
+using namespace std;
+
+Cat::Cat() {
+	cout << "Cat created." << endl;
+
+	happy = true;
+}
+
+Cat::~Cat() {
+	cout << "Cat destroyed." << endl;
+}
+
+void Cat::speak() {
+	if(happy) {
+		cout << "Meouww!" << endl;
+	}
+	else {
+		cout << "Ssssss!" << endl;
+	}
+}
