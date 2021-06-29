9a0edf0a99295695d23612b6af2d4f10fbf84b44
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-29 21:00:57 +0200

    Cpp - change main

---
+++ string_stream/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+
+	string name = "Paul";
+	register int age = 32;
+
+	stringstream ss;
+
+	ss << "Name is: ";
+	ss << name;
+	ss << "; Age is: ";
+	ss << age;
+
+	string info = ss.str();
+
+	cout << info << endl;
+
+	return 0;
+}
