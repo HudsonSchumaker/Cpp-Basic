0af0b7661a1dc805f845715655ee5afe1020eef0
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-29 20:51:45 +0200

    Cpp - change main

---
+++ Constructors/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Cat.hpp"
+using namespace std;
+
+int main() {
+	cout << "Starting program ..." << endl;
+
+	{
+		Cat bob;
+		bob.speak();
+	}
+
+	cout << "Ending program ..." << endl;
+	return 0;
+}
