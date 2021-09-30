6da2f095041603e050b821f72c30ce7b5f6d9b96
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-09-30 20:04:49 +0200

    Update Main.cpp

--- logical/Main.cpp
+++ logical/Main.cpp
@@ -18,7 +18,7 @@ bool False(std::string id) {
 int main() {
     bool result;
     // let's evaluate 3 booleans with || and && to illustrate operator precedence
-    //precedence does not mean that && will be evaluated first but rather where
+    // precedence does not mean that && will be evaluated first but rather where
     //parentheses would be added
     //example 1
     result = False("A") || False("B") && False("C");
