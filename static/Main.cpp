a4ab4806708b6282c781bd265631c23605aabf56
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-06-20 21:39:21 +0200

    Update Main.cpp

--- static/Main.cpp
+++ static/Main.cpp
@@ -7,15 +7,16 @@
 
 class Number {
     private:
-        static const int accessCount = 0;
+        static const int accessCount = 1;
 
     public:
-        static int zero(){return 0;}
+        static int zero() { return 0; }
         static int getCount() { return accessCount; }
 };
-
 
 int main() {
     Number::zero();
     std::cout << Number::getCount() << std::endl;
+    
+    return 0;
 }
