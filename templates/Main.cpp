2db86dbda1aa5cf112a680d48affcbb6eb7758dc
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-08-01 21:15:05 +0200

    Update Main.cpp

--- templates/Main.cpp
+++ templates/Main.cpp
@@ -5,12 +5,17 @@
 
 #include <iostream>
 
-template
+template <typename T, typename U>
+auto max(T a, U b) {
+  return (a > b) ? a : b;
+}
 
 
+int main() {
 
-
-int Main() {
-
+  std::cout << max(2, 3) << '\n';
+  std::cout << max(2, 3.3f) << '\n';
+  std::cout << max('Z', 'a') << '\n';
+  
   return 0;
 }
