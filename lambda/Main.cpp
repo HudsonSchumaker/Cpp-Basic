3cc8da5aee0af133275bda0a6fdb30bc1899c1c2
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-08-02 21:23:26 +0200

    Update Main.cpp

--- lambda/Main.cpp
+++ lambda/Main.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+void foreach(const std::vector<int>& values, void(*func)(int)) {
+   for (int value : values) {
+       func(value);
+   }
+}
+
+int main() {
+
+   std::vector values = { 1,5,4,2,3 };
+   foreach(values, [](int value) { std::cout << value << '\n';} );
+}
