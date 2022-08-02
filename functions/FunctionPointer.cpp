263741f8eae6a65946f04a4492e84c95a13a17cb
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-08-02 21:15:18 +0200

    Update FunctionPointer.cpp

--- functions/FunctionPointer.cpp
+++ functions/FunctionPointer.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+void foreach(const std::vector<int>& values, void(*func)(int)) {
+   for (int value : values) {
+       func(value);
+   }
+}
+
+void print(int value) {
+   std::cout << value << '\n';
+}
+
+int main() {
+
+   std::vector values = { 1,5,4,2,3 };
+   foreach(values, print);
+}
